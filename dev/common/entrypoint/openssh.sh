9cf09060bf9aedf434371e1dbe6acd47048fb3e8
mrblissfulgrin <jcb7777777@gmail.com>
2024-04-02 20:53:53 +0000

    feat: add github ssh key

--- dev/common/entrypoint/openssh.sh
+++ dev/common/entrypoint/openssh.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -xe
 
-echo "SSH_PATH=$PATH" > /home/$DEV_USER/.ssh/environment
+echo "SSH_PATH=$PATH" >/home/$DEV_USER/.ssh/environment
 service ssh restart
+su $DEV_USER --command eval "$(ssh-agent -s)" && [ -f ~/.ssh/github ] && ssh-add ~/.ssh/github
